32e865f1eda88c67d4223502c1fe46d337f70cb4
Rishita <burarishita@gmail.com>
2023-11-26 21:34:58 +0530

    electricity bill

---
+++ riks/q5-electricity/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electricity Bill</title>
+</head>
+<body>
+    <form action="elec.php" method="post"></form>
+    Account no : <input type="number" name="number">
+    Name : <input type="text" name="name">
+    Units: <input type="number" name="units">
+    <input type="submit" name="submit">
+
+    
+</body>
+</html>
